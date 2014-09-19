b247ce998240dd31a6aac2e4e02b2d8a4551598a
Ralf Westphal <info@ralfw.de>
2014-09-19 13:03:28 +0200

    Refactored: introduced dialog to declutter main()

--- exploration/Program.cs
+++ exploration/Program.cs
@@ -22,37 +22,16 @@ namespace consoledialogs
 			term.OnKey += canv.ProcessKey;
 			term.OnStarted += canv.Initialize;
 
-			Textbox tb;
-			tb = new Textbox (5, 7, 5);
-			tb.Name = "anrede";
-			tb.Text = "Herr";
-			canv.Add (tb);
-		
-			tb = new Textbox (5, 8, 20);
-			tb.Name = "nachname";
-			tb.Text = "Klöbner";
-			canv.Add (tb);
+			var dlg = new Dialog (canv);
 
-			Button b;
-			b = new Button (5, 10, "Begrüßen");
-			b.OnPressed += () => {
-				(canv["gruß"] as Label).Text = string.Format("Hallo, {0} {1}!", 
-														   (canv["anrede"] as Textbox).Text, 
-														   (canv["nachname"] as Textbox).Text);
+			dlg.Begrüßung_angefordert += (anrede, nachname) => {
+				var gruß = string.Format("Hallo, {0} {1}!", anrede, nachname);
+				dlg.Gruß_anzeigen(gruß);
 			};
-			canv.Add (b);
-
-
-			Label lb;
-			lb = new Label (5, 12) { Text = "Gruß: " };
-			canv.Add (lb);
-
-			lb = new Label (11, 12);
-			lb.Name = "gruß";
-			canv.Add (lb);
 
 			term.Run ();
 		}
 	}
 		
+
 }
